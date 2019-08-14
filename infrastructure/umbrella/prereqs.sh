8219ffd7ddc29d742454f6423aaca772ed80776d
Andrew <andrew@integral.io>
2019-08-14 16:52:01 -0400

    we don't need this

--- infrastructure/umbrella/prereqs.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-kubectl apply \
-    -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.9/deploy/manifests/00-crds.yaml
-
-kubectl create namespace cert-manager
-kubectl label namespace cert-manager certmanager.k8s.io/disable-validation=true
